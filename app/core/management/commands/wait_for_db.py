928ef580186df20ca5c9e950446ef91b4bfb1887
pipemohz <pipemoreno9405@gmail.com>
2022-08-30 16:50:54 -0500

    Fix minor typos and missing lines

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -13,7 +13,7 @@ class Command(BaseCommand):
     """Django command to wait for database."""
 
     def handle(self, *args, **options):
-        "Entrypoint for command."
+        """Entrypoint for command."""
         self.stdout.write("Waiting for database...")
         db_up = False
         while db_up is False:
@@ -24,4 +24,4 @@ class Command(BaseCommand):
                 self.stdout.write('Database unavailable, waiting 1 second...')
                 time.sleep(1)
 
-        self.stdout.write(self.style.SUCCESS('Database available!.'))
+        self.stdout.write(self.style.SUCCESS('Database available!'))
---
+++ app/core/tests/test_models.py
@@ -0,0 +1,36 @@
+"""
+Test for models.
+"""
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTests(TestCase):
+    """Test models."""
+
+    def test_create_user_with_email_successfull(self):
+        """Test creating a user with an email is successful."""
+
+        email = 'test@mail.com'
+        password = 'testpass123'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password,
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Test email is normalized for new users."""
+
+        sample_emails = [
+            ['test1@Mail.com', 'test1@mail.com'],
+            ['Test2@Mail.com', 'Test2@mail.com'],
+            ['TEST3@MAIL.COM', 'TEST3@mail.com'],
+            ['test4@example.COM', 'test4@example.com']
+        ]
+
+        for email, expected in sample_emails:
+            user = get_user_model().objects.create_user(email, 'sample123')
+            self.assertEqual(user.email, expected)
